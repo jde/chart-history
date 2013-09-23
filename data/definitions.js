54f5fc61d1321746b45ebc67c70ea1509240118a
David Erwin <davidjerwin@gmail.com>
2013-09-23 15:54:59 -0400

    Adding definitions

---
+++ data/definitions.js
@@ -0,0 +1,41 @@
+var marketDefinitions = [{
+	name: 'Municipal Bonds',
+	description: 'Debt issued by local governments and their agencies. Municipal bond issuers include states, cities, counties, public utilities and school districts.'
+},
+{
+	name: 'Corporate Bonds',
+	description: 'Debt issued by a company. The interest paid is usually higher than municipal bonds and Treasurys because this debt is considered to be more risky. The interest rates on corporate bonds is based on a company\'s ability to pay.'
+},
+{
+	name: 'U.S. Treasurys',
+	description: 'Debt issued by the U.S. government. Treasurys are generally considered to risk-free assets by investors because the federal government stands behind them with its taxing power to avoid default.'
+},
+{
+	name: 'Asset-Backed Securities',
+	description: 'Securities backed by the collateral of underlying assets. Such assets can include auto loans, credit card debt, mortgages or a company\'s accounts receivables.'
+},
+{
+	name: 'Initial Public Offerings',
+	description: 'The first sale of shares by a private company to the public markets. Most companies go public to gain capital to grow their businesses.'
+},
+{
+	name: 'Secondary Offerings',
+	description: 'New stock issued by a company that has already made its initial public offering. Companies conduct secondary offerings to expand their businesses, refinance their operations or provide liquidity to large shareholders who want to reduce their positions in the company.'
+}];
+
+var indicatorDefinitions = [{
+	name: 'Yield on 10-year Treasury Note',
+	description: 'The return that investors receive on a debt obligation issued by the U.S. government that matures in 10 years. The rate serves as a benchmark for other debt, such as corporate bonds and mortgages.'
+},
+{
+	name: 'Standard & Poor\'s 500-stock index',
+	description: 'An index of 500 stocks picked by Standard & Poor\'s to reflect the largest U.S. stocks by market value. The S&P 500 is a top benchmark for the overall U.S. stock market.'
+},
+{
+	name: 'Gold Price Per Ounce',
+	description: 'Many investors consider gold a hedge against inflation and market risk. Gold prices reflect the value of the physical asset as well as investors’ expectations about inflation and market volatility.'
+},
+{
+	name: '30-Year Fixed-Rate Mortgage',
+	description: 'A mortgage that has a fixed interest rate for the entire 30-year term of the loan. A 30-year fixed rate mortgage is the most common type of home loan and is used as a benchmark for the health of mortgage lending.'
+}];
---
+++ data/parseArticles.php
@@ -0,0 +1,43 @@
+<?php
+
+$in = file_get_contents('articles.txt');
+$out = array();
+
+$thisEra = "";
+$eras = explode("\n\n\n", $in);
+
+foreach ($eras as $era) {
+
+	$thisEra = array();
+
+	$parts = explode("\n\n", $era);
+
+	$headers = explode("\n", array_shift($parts));
+
+	$thisEra['title'] = $headers[0];
+	$thisEra['description'] = $headers[1];
+	$thisEra['start'] = (int) $headers[2];
+	$thisEra['end'] = (int) $headers[3];
+
+	$thisEra['articles'] = array();
+
+	while ($article = array_shift($parts)) {
+		
+		$thisArticle = explode("\n", $article);
+
+		array_push($thisEra['articles'], array(
+			'date' => $thisArticle[0],
+			'title' => $thisArticle[1],
+			'url' => $thisArticle[2],
+			'year' => (int) date("Y", strtotime($thisArticle[0]))
+		));
+	}
+
+	array_push($out, $thisEra);
+
+}
+
+$out = json_encode($out);
+
+echo $out;
+file_put_contents('data.json', $out);
